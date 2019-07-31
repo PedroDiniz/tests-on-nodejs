c06e9716dc069a17fb2497673ac6638834794a44
PedroDiniz <pedro_nork@hotmail.com>
2019-07-31 15:46:01 -0300

    initial-commit

---
+++ src/config/database.js
@@ -0,0 +1,16 @@
+require('../bootstrap');
+
+module.exports = {
+  dialect: process.env.DB_DIALECT || 'postgres',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  storage: './__tests__/database.sqlite',
+  logging: false, // uncomment this line if you want to se the logs
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
